11e6754077329d144ef5778d91fc80700d468d43
Gerard Garcia <gerardgarcia1908@gmail.com>
2019-12-11 14:22:30 +0100

    Millores frontend

--- src/pages/DragAndDropPage/DragAndDropPage.js
+++ src/pages/DragAndDropPage/DragAndDropPage.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 //import AppBar_Component from './../../Components/AppBar_Component/AppBar_Component';
 import Horizontal from '../../Components/horizBar/horizBar';
 import AppBar from '../../Components/AppBar/AppBar';
-import Container from '@material-ui/core/Container';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Button from '@material-ui/core/Button';
-import { Grid, ListItem, Typography, List, ListItemText, Paper } from '@material-ui/core';
+import {CssBaseline, Container, Grid, ListItem, Typography, Paper, Button } from '@material-ui/core';
+import {List, ListItemText, Avatar, ListItemAvatar, ListItemSecondaryAction} from '@material-ui/core';
+
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+
 const { ipcRenderer } = window.require("electron");
 
 
@@ -29,8 +32,20 @@ export class DragAndDropPage extends Component {
                             console.log(value);
                             return (
                                 <ListItem key={idx}>
-                                    <ListItemText key={idx} primary={value} />
-                                    <img className="ThumbnailStyle" src={value} />
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                        <FolderIcon />
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText
+                                        primary={<Typography>{idx+1}:&nbsp;{value}</Typography>}
+                                        style={{'word-break': 'break-all'}}
+                                    />
+                                    <ListItemSecondaryAction>
+                                        <IconButton edge="end" aria-label="delete" onClick={() => this.deleteItemFromFiles(idx)}>
+                                        <DeleteIcon />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
                                 </ListItem>
                             )
                         })
@@ -40,24 +55,23 @@ export class DragAndDropPage extends Component {
         }
     }
 
-
+    deleteItemFromFiles(idx){
+        let list = this.state.files;
+        list.splice(idx, 1);
+        this.setState({files: list})
+    }
     componentDidMount() {
 
-
         var holder = document.getElementById('dropbox');
-
         holder.ondragover = () => {
             return false;
         };
-
         holder.ondragleave = () => {
             return false;
         };
-
         holder.ondragend = () => {
             return false;
         };
-
         holder.ondrop = (e) => {
             e.preventDefault();
 
@@ -98,8 +112,8 @@ export class DragAndDropPage extends Component {
     }
 
     sendOrthanc() {
-        if (this.state.pacs === false) {
-            alert('select pacs');
+        if (this.state.files === false) {
+            alert('Drag files.');
         }
         else {
             console.log("PreUpload:",this.state.pacs)
@@ -117,33 +131,21 @@ export class DragAndDropPage extends Component {
             this.setState({ pacs: value })
     }
 
-
     render() {
-
-        const dragDropStyle = {
-            position: 'relative',
-            'margin-right': 'auto',
-            'margin-left': 'auto',
-            height: '200px',
-            width: '700px',
-            'margin-bottom': '20px',
-            'margin-top': '15px',
-
-        }
         return (
             <CssBaseline>
                 <AppBar page="Anonimizer" history={this.props.history} />
                 <Container fixed>
                     <Container container maxWidth="sm" >
                         <Grid container id="dropbox" >
-                            <div style={dragDropStyle} className="draggable">
-                                <Paper elevation={4} /* className={classes.paperClone} */ style={{ height: '200px', width: '400px', textAlign: 'center' }}>
+                            <div style={styles.dragDropStyle} className="draggable">
+                                <Paper elevation={4} /* className={classes.paperClone} */ style={{ height: '200px', textAlign: 'center' }}>
                                     drag files here
                                 </Paper>
                             </div>
                         </Grid>
                         <Horizontal pacsValue={this.pacsValue.bind(this)} />
-                        <div style={{ margin: 'auto' }}>
+                        <div style={{ margin: 'auto', marginTop: '20px' }}>
                             <Button variant="contained" color="primary" className="buttonPrimary" onClick={() => this.Anonimize('conda')}>Anonimize</Button>
                             <Button variant="contained" color="secondary" className="buttonSecondary" onClick={() => this.sendOrthanc()}>Send Orthanc</Button>
                         </div>
@@ -162,4 +164,15 @@ export class DragAndDropPage extends Component {
             </CssBaseline >
         )
     }
+}
+const styles = {
+    dragDropStyle: {
+        position: 'relative',
+        'margin-right': 'auto',
+        'margin-left': 'auto',
+        width: '100%',
+        'margin-bottom': '20px',
+        'margin-top': '15px',
+
+    },
 }

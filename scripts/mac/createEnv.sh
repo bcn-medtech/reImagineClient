138b2c871ced99e5123d481ab75c217c6bc852c5
Mario Ceresa <mrceresa@gmail.com>
2023-01-04 17:56:45 +0100

    Fixed permissions and minor errors

--- scripts/mac/createEnv.sh
+++ scripts/mac/createEnv.sh
@@ -6,12 +6,11 @@ envname="$3"
 source $condapath $condahome
 
 echo "Creating env..."
-conda create -n $envname python=3.7 mamba -y
+conda create -n $envname python=3.7 sqlalchemy -y
 
-mamba install --nanme $envname sqlalchemy -y
-mamba install --name $envname -c SimpleITK SimpleITK=1.2.4 -y
-mamba install --name $envname -c conda-forge pydicom=1.2.1 -y
-mamba install --name $envname -c conda-forge deid=0.1.42 -y
+conda install --name $envname -c SimpleITK SimpleITK=1.2.4 -y
+conda install --name $envname -c conda-forge pydicom=1.2.1 -y
+conda install --name $envname -c conda-forge deid=0.1.42 -y
 #   conda install --name deid -c https://services.simbiosys.upf.edu/conda/ -c conda-forge gdcm -y
    
 
